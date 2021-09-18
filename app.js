66cb123235678288fb6b5a60f2bb751626aac2e2
Konstaphy <Konstaphy@gmail.com>
2021-09-18 14:17:24 +0500

    repaired heroku deploy

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html"))
 })
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, "/index2.html"))
+})
+
 const start = () => {
     app.listen(PORT, () => {
         console.log('Server started on port ' + PORT + '...')
