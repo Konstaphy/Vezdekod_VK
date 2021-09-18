aa47ac65cf26a0ead4b090c03412522fe5447f1c
Konstaphy <Konstaphy@gmail.com>
2021-09-18 14:02:16 +0500

    repaired

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 
 const PORT = process.env.PORT || 80
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/index.html"))
 })
 
