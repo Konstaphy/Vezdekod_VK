da58202b58c011228dbe8339982e4bc106d1319e
Konstaphy <Konstaphy@gmail.com>
2021-09-18 18:31:29 +0500

    third task

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require("path");
 require('dotenv').config()
+const cors = require('cors')
+
+const router = require('./router/Router')
 
 const app = express()
 
 const PORT = process.env.PORT || 80
+
+app.use(express.json())
+app.use(cors({
+    credentials: true
+}))
+
+app.use('/api', router, () => {
+    console.log('router worked')
+})
 
 app.use(express.static(__dirname + '/build'));
 
---
+++ router/Router.js
@@ -0,0 +1,34 @@
+const {Router} = require('express')
+const fs = require("fs");
+const path = require('path')
+const router = new Router()
+
+router.post('/saveUser', (req, res) => {
+    try{
+        const {name, surname, email, company, city, msg} = req.body
+
+        // validation
+
+        const pathNeeded = path.join(__dirname, "response.txt")
+
+        // add to text file
+        try {
+            if (fs.existsSync(pathNeeded)){
+                fs.appendFileSync('response.txt', `${name} ${surname} with email: ${email}, works in ${company}, lives in ${city} sent message ${msg} \n\n`)
+            } else {
+                const CreateFiles = fs.createWriteStream('response.txt', {
+                    flags: 'a'
+                })
+                CreateFiles.write(`${name} ${surname} with email: ${email}, works in ${company}, lives in ${city} sent message ${msg} \n\n`)
+            }
+        } catch (e) {
+          return Error
+        }
+
+        res.status(200).send()
+    } catch (e) {
+        res.send({"error": e.toString()}).status(400)
+    }
+})
+
+module.exports = router
